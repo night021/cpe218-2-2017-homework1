714b7981e9c006279ad603e13ada8ed9bfad2158
night021 <night.wootthipong@hotmail.com>
2018-01-24 21:59:31 +0700

    Update Homework1.java

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,16 +1,77 @@
+import java.util.Stack;
+import java.util.Scanner;
 
 public class Homework1 {
 
-	public static void main(String[] args) {
-		// Begin of arguments input sample
-		if (args.length > 0) {
-			String input = args[0];
-			if (input.equalsIgnoreCase("251-*32*+")) {
-				System.out.println("(2*(5-1))+(3*2)=14");
-			}
-		}
-		// End of arguments input sample
-		
-		// TODO: Implement your project here
-	}
+public static Node Tree;
+public static Stack<Character> Temp = new Stack<Character>();  
+
+public static class Node{
+        Node left;
+        Node right;
+        Character key;
+    public Node c;
+
+    public Node(char n) {
+            key = n;
+        }
+        public  String toString(){
+            return key.toString();
+        }
+}
+  
+    public static void main(String[] args){
+        System.out.print("Input : ");
+        Scanner inputs = new Scanner(System.in);
+        String input =  inputs.nextLine();
+
+        for(int i=0;i<input.length();i++){
+            Temp.add(input.charAt(i));
+        }
+        Tree = new Node(Temp.pop());
+        infix(Tree);
+        inorder(Tree);
+        System.out.print("=" + calculate(Tree));
+        TreeDemo.main(args);
+    }
+
+    public static void infix(Node n) {
+
+                    if (n.key == '+' || n.key == '-' || n.key == '*' || n.key == '/') {
+                            n.right = new Node(Temp.pop());
+                            infix(n.right);
+                            n.left = new Node(Temp.pop());
+                            infix(n.left);
+                    }
+    }
+    
+    public static void inorder(Node n){
+        if (n.key == '+' || n.key == '-' || n.key == '*' || n.key == '/'){
+                if(n!=Tree){
+                        System.out.print("(");
+                    }
+                    inorder(n.left);
+                    System.out.print(n.key);
+                    inorder(n.right);
+                if(n!=Tree){
+                    System.out.print(")");
+                    }
+                }else {
+                            System.out.print(n.key);
+                    }
+    }
+   
+    public static int calculate(Node n) {
+            switch (n.key) {
+                case '+':
+                    return calculate(n.left) + calculate(n.right);
+                case '-':
+                    return calculate(n.left) - calculate(n.right);
+                case '/':
+                    return calculate(n.left) / calculate(n.right);
+                case '*':
+                    return calculate(n.left) * calculate(n.right);
+            }        
+            return Integer.parseInt(n.key.toString());
+    }
 }
